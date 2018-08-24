211be1119aa95a50e99f5e5f20f31e768328e121
gerbidev <gerbi@protonmail.com>
2018-08-24 16:00:13 -0700

    update model and controller categories

--- resources/views/principal.blade.php
+++ resources/views/principal.blade.php
@@ -8,16 +8,18 @@
     <meta name="description" content="Sistema Ventas Laravel Vue Js- IncanatoIT">
     <meta name="author" content="Incanatoit.com">
     <meta name="keyword" content="Sistema ventas Laravel Vue Js, Sistema compras Laravel Vue Js">
-    <link rel="shortcut icon" href="img/favicon.png">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="shortcut icon" href="gl/img/favicon.png">
     <title>Sistema Ventas - IncanatoIT</title>
-    <!-- Icons -->
-    <link href="css/font-awesome.min.css" rel="stylesheet">
-    <link href="css/simple-line-icons.min.css" rel="stylesheet">
-    <!-- Main styles for this application -->
-    <link href="css/style.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="gl/css/font-awesome.min.css">
+    <link rel="stylesheet" href="gl/css/simple-line-icons.min.css">
+    <link rel="stylesheet" href="gl/css/style.css">
+
 </head>
 
 <body class="app header-fixed sidebar-fixed aside-menu-fixed aside-menu-hidden">
+<div id="app">
 <header class="app-header navbar">
     <button class="navbar-toggler mobile-sidebar-toggler d-lg-none mr-auto" type="button">
         <span class="navbar-toggler-icon"></span>
@@ -56,7 +58,7 @@
         </li>
         <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle nav-link" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
-                <img src="img/avatars/6.jpg" class="img-avatar" alt="admin@bootstrapmaster.com">
+                <img src="gl/img/avatars/6.jpg" class="img-avatar" alt="admin@bootstrapmaster.com">
                 <span class="d-md-down-none">admin </span>
             </a>
             <div class="dropdown-menu dropdown-menu-right">
@@ -71,104 +73,33 @@
 </header>
 
 <div class="app-body">
-    <div class="sidebar">
-        <nav class="sidebar-nav">
-            <ul class="nav">
-                <li class="nav-item">
-                    <a class="nav-link active" href="main.html"><i class="icon-speedometer"></i> Escritorio</a>
-                </li>
-                <li class="nav-title">
-                    Mantenimiento
-                </li>
-                <li class="nav-item nav-dropdown">
-                    <a class="nav-link nav-dropdown-toggle" href="#"><i class="icon-bag"></i> Almacén</a>
-                    <ul class="nav-dropdown-items">
-                        <li class="nav-item">
-                            <a class="nav-link" href="#"><i class="icon-bag"></i> Categorías</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#"><i class="icon-bag"></i> Artículos</a>
-                        </li>
-                    </ul>
-                </li>
-                <li class="nav-item nav-dropdown">
-                    <a class="nav-link nav-dropdown-toggle" href="#"><i class="icon-wallet"></i> Compras</a>
-                    <ul class="nav-dropdown-items">
-                        <li class="nav-item">
-                            <a class="nav-link" href="i#"><i class="icon-wallet"></i> Ingresos</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#"><i class="icon-notebook"></i> Proveedores</a>
-                        </li>
-                    </ul>
-                </li>
-                <li class="nav-item nav-dropdown">
-                    <a class="nav-link nav-dropdown-toggle" href="#"><i class="icon-basket"></i> Ventas</a>
-                    <ul class="nav-dropdown-items">
-                        <li class="nav-item">
-                            <a class="nav-link" href="i#"><i class="icon-basket-loaded"></i> Ventas</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#"><i class="icon-notebook"></i> Clientes</a>
-                        </li>
-                    </ul>
-                </li>
-                <li class="nav-item nav-dropdown">
-                    <a class="nav-link nav-dropdown-toggle" href="#"><i class="icon-people"></i> Acceso</a>
-                    <ul class="nav-dropdown-items">
-                        <li class="nav-item">
-                            <a class="nav-link" href="i#"><i class="icon-user"></i> Usuarios</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#"><i class="icon-user-following"></i> Roles</a>
-                        </li>
-                    </ul>
-                </li>
-                <li class="nav-item nav-dropdown">
-                    <a class="nav-link nav-dropdown-toggle" href="#"><i class="icon-pie-chart"></i> Reportes</a>
-                    <ul class="nav-dropdown-items">
-                        <li class="nav-item">
-                            <a class="nav-link" href="i#"><i class="icon-chart"></i> Reporte Ingresos</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#"><i class="icon-chart"></i> Reporte Ventas</a>
-                        </li>
-                    </ul>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="main.html"><i class="icon-book-open"></i> Ayuda <span class="badge badge-danger">PDF</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="main.html"><i class="icon-info"></i> Acerca de...<span class="badge badge-info">IT</span></a>
-                </li>
-            </ul>
-        </nav>
-        <button class="sidebar-minimizer brand-minimizer" type="button"></button>
-    </div>
+
+    @include('plantilla.sidebar')
 
     <!-- Contenido Principal -->
     @yield('contenido')
-
-
     <!-- /Fin del contenido principal -->
 </div>
 
-
+</div>
 
 <footer class="app-footer">
-    <span><a href="http://www.incanatoit.com/">IncanatoIT</a> &copy; 2017</span>
+    <span><a href="t.me/gerbi">Gerbi Dev</a> &copy; 2017</span>
     <span class="ml-auto">Desarrollado por <a href="http://www.incanatoit.com/">IncanatoIT</a></span>
+    <p>Entonces</p>
 </footer>
 
-<!-- Bootstrap and necessary plugins -->
-<script src="js/jquery.min.js"></script>
-<script src="js/popper.min.js"></script>
-<script src="js/bootstrap.min.js"></script>
-<script src="js/pace.min.js"></script>
-<!-- Plugins scripts required by all views -->
-<script src="js/Chart.min.js"></script>
-<!-- GenesisUI main scripts -->
-<script src="js/template.js"></script>
+<script src="{{ asset('js/app.js') }}"></script>
+<script src="gl/js/jquery.min.js"></script>
+<script src="gl/js/popper.min.js"></script>
+<script src="gl/js/bootstrap.min.js"></script>
+<script src="gl/js/pace.min.js"></script>
+<script src="gl/js/Chart.min.js"></script>
+<script src="gl/js/template.js"></script>
+
+
+
+
 </body>
 
 </html>
