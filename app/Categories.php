2d70de9af087645cdba6e2d109a81955dcb785ac
Gerbi <gerbi@protonmail.com>
2018-08-26 16:43:19 -0700

    Model - Articulos

--- app/Categories.php
+++ app/Categories.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Categories extends Model
 {
     protected $fillable = ['nombre','descripcion','condicion'];
+
+    public function articulos()
+    {
+        return $this->hasMany('App\Articulo');
+    }
 }
